d7ee8b653c3cb3da7757f370407de82192f6ca38
Roman Svityuk <ke1717srs13@susu.ru>
2021-04-18 23:08:37 +0500

    Новый файл mycomplex.h

--- mycomplex.h
+++ mycomplex.h
@@ -1,39 +1,49 @@
 #ifndef _MY_COMPLEX_H_
-#define _MY_COMPLEX_H_
+#define _MY_COMPLEX_H_///<Library for working with complex numbers
 #include <iostream>
+
 using namespace std;
-class Complex
-{double Re;
-	double Im; 
-	public:
-Complex(double aRe=0, double aIm=0);
-Complex(const Complex&);
-~Complex(    );
-void Set(double aRe, double aIm=0);
-operator double();
- double abs();
- friend istream& operator>>(   istream&,Complex&) ; 
 
+class Complex//class creation Complex
 
+{
+      double Re;
+	  double Im;
+public:
 
-friend ostream& operator<< (ostream&, Complex & );
+      Complex (double aRe = 0, double aIm = 0);///Create complex number
+      Complex (const Complex & );
+     ~Complex();
 
-Complex operator+ (const Complex&);
-Complex operator- (const Complex&);
-Complex operator+ (const double&);
-friend 		Complex operator+ (const double&, const Complex&);
-Complex operator- (const double&);
-friend Complex operator- (const 	double&, const Complex&);
-Complex operator* (const Complex&);
-Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
+void Set (double aRe, double aIm = 0);/// Assigns the complex number a new value.
+      operator double();
+      double abs();
 
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+friend istream & operator >> (istream &, Complex &);
+friend ostream & operator << (ostream &, Complex &);
+
+      Complex operator + (const Complex &);
+      Complex operator - (const Complex &);
+      Complex operator + (const double &);
+
+friend Complex operator + (const double &, const Complex &);
+      Complex operator - (const double &);
+
+friend Complex operator - (const 	double &, const Complex &);
+      Complex operator * (const Complex &);
+      Complex operator * (const double &);
+
+friend Complex operator * (const double &, const Complex &);
+      Complex operator / (const double &);
+
+      Complex & operator += (const Complex &);/// Adds a number to the target complex number, where the number to add is complex.
+      Complex & operator -= (const Complex &);
+      Complex & operator *= (const Complex &);
+      Complex & operator += (const double &);
+      Complex & operator -= (const double &);
+      Complex & operator *= (const double &);
+      Complex & operator /= (const double &);
+      Complex & operator = (const Complex &);
+      Complex & operator = (const double &);
+};
 #endif
