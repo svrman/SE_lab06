d144caa1f146a23b20e87bed5e0e5743ac0059ed
Svityuk Roman <48216527+svrman@users.noreply.github.com>
2021-04-25 20:19:48 +0500

    Update mycomplex.h

--- mycomplex.h
+++ mycomplex.h
@@ -1,47 +1,77 @@
 #ifndef _MY_COMPLEX_H_
-#define _MY_COMPLEX_H_							//имя подключаемого файла "mycomplex.h"
+#define _MY_COMPLEX_H_
 #include <iostream>
-
 using namespace std;
 
-class Complex								//объявление класса "комплексное число"
-
-{
-      	double Re;							//объявление параметров действительной части(закрытая переменная)
-	    double Im;							//объявление параметров мнимой части(закрытая переменная)
-public:									//тип доступа к описаным ниже методам(общий доступ)
-
-      Complex (double aRe = 0, double aIm = 0);				//объявление конструктра записи изменяемой действительной и мнимой части(Re и Im)
-      Complex (const Complex &);					//объявление конструктора копирования
-     ~Complex();							//объявление деструтора, очистка памяти
-
-void Set(double aRe, double aIm = 0);					//объявление метода сеттера
-      operator double();						//объявление operator
-      double abs();							//объявление метода вычисления модуля комплексного числа
-
-friend istream & operator >> (istream &, Complex &);		//объявление дружественных операторов, которым мы разрешаем доступ
-friend ostream & operator << (ostream &, Complex &);		//к закрытым переменным
-
-      Complex operator + (const Complex &);				//объявление оператора сложения, комплексная переменная
-      Complex operator - (const Complex &);				//объявление оператора вычитания, комплексная переменная
-      Complex operator + (const double &);				//объявление оператора сложения,  действительная переменная
-
-friend Complex operator + (const double &, const Complex &); 	//объявление дружественного оператора сложения, одна действительная переменная и одная комплексная переменная
-       Complex operator - (const double &);				//объявление оператора вычитания, действительная переменная
-friend Complex operator - (const 	double &, const Complex &);	//объявление дружественного оператора вычитания, одна действительная переменная и одная комплексная переменная
-       Complex operator * (const Complex &);				//объявление оператора умножение, комплексная переменная
-       Complex operator * (const double &);				//объявление оператора умножения, действительная переменная
-friend Complex operator * (const double &, const Complex &);	//объявление дружественного оператора умножения, одна действительная переменная и одная комплексная переменная
-       Complex operator / (const double &);				//объявление оператора деления, действительная переменная
-
-       Complex & operator += (const Complex &);				//объявление оператора +=, комплексная переменная
-       Complex & operator -= (const Complex &);				//объявление оператора -=, комплексная переменная
-       Complex & operator *= (const Complex &);				//объявление оператора *=, комплексная переменная
-       Complex & operator += (const double &);				//объявление оператора +=, действительная переменная
-       Complex & operator -= (const double &);				//объявление оператора -=, действительная переменная
-       Complex & operator *= (const double &);				//объявление оператора *=, действительная переменная
-       Complex & operator /= (const double &);				//объявление оператора /=, действительная переменная
-       Complex & operator = (const Complex &);				//объявление оператора присванивания, комплексная переменная
-       Complex & operator = (const double &);				//объявление оператора присванивания, действительная переменная
+/** @brief Creating class of Complex Value
+ *@param aRe - Real part of complex value
+ *@param aIm - Imagine part of complex value
+ */
+class Complex {
+/**@param Re - Real part of complex value
+*/
+  double Re;
+ /**@param Im - Imagine part of complex value
+*/
+  double Im;
+  public:
+    /**@brief Complex Value class constructor
+     */
+	Complex(double aRe = 0, double aIm = 0);
+	Complex(const Complex &);
+	/**@brief Complex Value class destructor
+	*/
+	~Complex();
+	void Set(double aRe, double aIm = 0);
+	operator double();
+	/**@return Return abs of complex value
+	*/
+	double abs();
+	/**@brief Friend Operator for inserting complex value in stream
+    */
+	friend istream & operator >> (istream &, Complex &);
+	/**@brief Friend Operator for getting complex value from stream
+    */
+	friend ostream & operator << (ostream &, Complex &);
+	/**@brief Operator sums one complex value with other
+	*/
+	Complex operator + (const Complex &);
+	/**@brief Operator substracts one complex value from other
+	*/
+	Complex operator - (const Complex &);
+	/**@brief Operator sums real value to complex value
+	*/
+	Complex operator + (const double );
+	/**@brief Friend Operator for summinh two complex values
+	*/
+	friend Complex operator + (const double &, const Complex &);
+	/**@brief Operator substracts real value from complex value
+	*/
+	Complex operator - (const double &);
+	/**@brief Friend Operator substracts one complex value from other
+	*/
+	friend Complex operator - (const double &, const Complex &);
+	/**@brief Operator for multiplying one complex value to other
+	*/
+	Complex operator * (const Complex &);
+	/**@brief Operator for multiplying complex value on real value
+	*/
+	Complex operator * (const double &);
+	/**@brief Friend Operator for multiplying two complex values
+	*/
+	friend Complex operator * (const double &, const Complex &);
+	/**@brief Operator divides one complex value by real value
+	*/
+	Complex operator / (const double &);
+	// Some additional operators
+	Complex & operator += (const Complex &);
+	Complex & operator -= (const Complex &);
+	Complex & operator *= (const Complex &);
+	Complex & operator += (const double &);
+	Complex & operator -= (const double &);
+	Complex & operator *= (const double &);
+	Complex & operator /= (const double &);
+	Complex & operator = (const Complex &);
+	Complex & operator = (const double &);
 };
 #endif
